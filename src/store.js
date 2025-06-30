25e7afa82f9ac74f7bc3a395f45b7c4c9272b94f
Abhishek Kumar <abhishekkumar@Abhisheks-MacBook-Air.local>
2025-06-30 16:10:18 +0530

    Updated auth and added loading state

--- src/store.js
+++ src/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import { shopApi } from "./features/shops/shopApi";
 import { authApi } from "./features/auth/authApi";
 import userReducer from "./features/auth/userSlice"
+import walletReducer from "./features/auth/walletSlice"
 
 export const store = configureStore({
   reducer: {
     [shopApi.reducerPath]: shopApi.reducer,
     [authApi.reducerPath]: authApi.reducer,
-    user : userReducer
+    user : userReducer,
+    wallet: walletReducer, 
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(shopApi.middleware, authApi.middleware),
